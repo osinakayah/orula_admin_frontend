eb36c0d08a641eeba80c608484cea6c2ba926b6f
daniel.abayomi <daniel.abayomi@arkounting.com.ng>
2019-07-11 08:14:47 +0100

    Table Populated and style changes

--- src/App/components/ModalComponent.js
+++ src/App/components/ModalComponent.js
@@ -1,17 +1,15 @@
 //ModalComponent.js
 import React from 'react';
+import {Card} from 'react-bootstrap';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Badge } from 'reactstrap';
 import Aux from "../../hoc/_Aux";
 class ModalComponent extends React.Component {
 
     state = {
-         modal: false, name: '', team :'' , country: ''};
+         modal: false};
 
     toggle = this.toggle.bind(this);
-    handleChangeName = this.handleChangeName.bind(this);
-    handleChangeTeam = this.handleChangeTeam.bind(this);
-    handleChangeCountry = this.handleChangeCountry.bind(this);
-    handleSubmit = this.handleSubmit.bind(this);
+
 
 
   toggle() {
@@ -19,58 +17,182 @@ class ModalComponent extends React.Component {
       modal: !this.state.modal
     });
   }
-  handleChangeName(event) {
-    this.setState({name: event.target.value});
-  }
-  handleChangeTeam(event) {
-    this.setState({team: event.target.value});
-  }
-  handleChangeCountry(event) {
-    this.setState({country: event.target.value});
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-     }
-
 
   render() {
     return (
 
         <Aux>
-          {/* <h1>React Bootstrap Modal Example</h1> */}
-        {/* <Button color="success" size="sm" onClick={this.toggle}>Add New Vendor</Button> */}
         <Badge variant="warning" size="sm" onClick={this.toggle}>View</Badge>
         <Modal isOpen={this.state.modal} size="lg">
         <form onSubmit={this.handleSubmit}>
-          <ModalHeader>Create Vendor</ModalHeader>
+          <ModalHeader>Delivery Details</ModalHeader>
           <ModalBody>
+          <h5 className="mb-4 text-c-red"><i className="feather icon-user-check text-c-red f-20 m-r-5"/> Sender's Information</h5>
           <div className="row">
-                <div className="form-group col-md-6">
-                    <label>Name:</label>
-                    <input type="text" value={this.state.name} onChange={this.handleChangeName} className="form-control" />
+                <div className="col-md-6">
+                  <Card>
+                      <Card.Body>
+                          <h6 className='mb-4'><i className="feather icon-user text-c-green f-20 m-r-5"/> Pick-up Info</h6>
+                          <div className="row d-flex align-items-center">
+                            <div className="col-4 text-right">
+                                <h6 className="f-w-300 m-b-0">Name:</h6>
+                            </div>
+
+                            <div className="col-8 text-left">
+                                <p className="m-b-0">Daniel Abayomi</p>
+                            </div>
+                          </div>
+                          <div className="row d-flex align-items-center">
+                            <div className="col-4 text-right">
+                                <h6 className="f-w-300 m-b-0">Phone:</h6>
+                            </div>
+
+                            <div className="col-8 text-left">
+                                <p className="m-b-0">09031822660</p>
+                            </div>
+                          </div>
+                          <hr/>
+                          <div className="row d-flex align-items-center">
+                              <div className="col-4 text-right">
+                                  <h6 className="f-w-300 m-b-0">Address:</h6>
+                              </div>
+
+                              <div className="col-8 text-left">
+                                  <p className="m-b-0">71, Adekunle street, yaba Lagos</p>
+                              </div>
+                          </div>
+                          <div className="progress m-t-30" style={{height: '3px'}}>
+                              <div className="progress-bar progress-c-theme" role="progressbar" style={{width: '100%'}} aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"/>
+                          </div>
+                      </Card.Body>
+                  </Card>
                 </div>
                 <div className="col-md-6">
-                    <label>Team:</label>
-                    <input type="text" value={this.state.team} onChange={this.handleChangeTeam} className="form-control" />
+                  <Card>
+                      <Card.Body>
+                          <h6 className='mb-4'><i className="feather icon-user text-c-green f-20 m-r-5"/> Receiver's Info</h6>
+                          <div className="row d-flex align-items-center">
+                            <div className="col-4 text-right">
+                                <h6 className="f-w-300 m-b-0">Name:</h6>
+                            </div>
+
+                            <div className="col-8 text-left">
+                                <p className="m-b-0">Daniel Abayomi</p>
+                            </div>
+                          </div>
+                          <div className="row d-flex align-items-center">
+                            <div className="col-4 text-right">
+                                <h6 className="f-w-300 m-b-0">Phone:</h6>
+                            </div>
+
+                            <div className="col-8 text-left">
+                                <p className="m-b-0">09031822660</p>
+                            </div>
+                          </div>
+                          <hr/>
+                          <div className="row d-flex align-items-center">
+                              <div className="col-4 text-right">
+                                  <h6 className="f-w-300 m-b-0">Address:</h6>
+                              </div>
+
+                              <div className="col-8 text-left">
+                                  <p className="m-b-0">16, Fasanya street, fadeyi Lagos.</p>
+                              </div>
+                          </div>
+                          <div className="progress m-t-30" style={{height: '3px'}}>
+                              <div className="progress-bar progress-c-theme" role="progressbar" style={{width: '100%'}} aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"/>
+                          </div>
+                      </Card.Body>
+                  </Card>
                 </div>
             </div>
+            <h5 className="mb-4 text-c-red"><i className="feather icon-user-check text-c-red f-20 m-r-5"/> Delivery Information</h5>
             <div className="row">
-             <div className="form-group col-md-4">
-            <label>Team:</label>
-                <input type="text" value={this.state.team} onChange={this.handleChangeTeam} className="form-control" />
-               </div>
+              <div className="col-md-6">
+              <Card>
+                  <Card.Body>
+                      <h6 className='mb-4'><i className="feather icon-user text-c-green f-20 m-r-5"/> Vendor's Info</h6>
+                      <div className="row d-flex align-items-center">
+                        <div className="col-4 text-right">
+                            <h6 className="f-w-300 m-b-0">Name:</h6>
+                        </div>
+
+                        <div className="col-8 text-left">
+                            <p className="m-b-0">Daniel Abayomi</p>
+                        </div>
+                      </div>
+                      <div className="row d-flex align-items-center">
+                        <div className="col-4 text-right">
+                            <h6 className="f-w-300 m-b-0">Phone:</h6>
+                        </div>
+
+                        <div className="col-8 text-left">
+                            <p className="m-b-0">09031822660</p>
+                        </div>
+                      </div>
+
+                      <div className="row d-flex align-items-center">
+                        <div className="col-4 text-right">
+                            <h6 className="f-w-300 m-b-0">Vehicle:</h6>
+                        </div>
+
+                        <div className="col-8 text-left">
+                            <p className="m-b-0">Toyota Corolla 2018</p>
+                        </div>
+                      </div>
+
+                      <hr/>
+                      <div className="row d-flex align-items-center">
+                        <div className="col-6 text-right">
+                            <h6 className="f-w-300 m-b-0">Total Deliveries:</h6>
+                        </div>
+
+                        <div className="col-6 text-left">
+                            <p className="m-b-0 f-w-600">32</p>
+                        </div>
+                      </div>
+                      
+                      <div className="progress m-t-30" style={{height: '3px'}}>
+                          <div className="progress-bar progress-c-theme" role="progressbar" style={{width: '100%'}} aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"/>
+                      </div>
+                  </Card.Body>
+              </Card>
               </div>
-            <div className="row">
+
+              <div className="col-md-6">
+              <Card>
+                  <Card.Body>
+                      <h6 className='mb-4'><i className="feather icon-user text-c-green f-20 m-r-5"/> Delivery update</h6>
+                      <div className="row d-flex align-items-center">
+                        <div className="col-4 text-right">
+                            <h6 className="f-w-300 m-b-0">Pickup Time:</h6>
+                        </div>
+
+                        <div className="col-8 text-left">
+                            <p className="m-b-0">27/07/19 10:53am</p>
+                        </div>
+                      </div>
+                      <div className="text-center" style={{marginTop: "20px"}}>
+                        <button type="button" className="btn btn-warning btn-sm">Track Vendor</button>
+                      </div>
+
+                      <div className="progress m-t-30" style={{height: '3px'}}>
+                          <div className="progress-bar progress-c-theme" role="progressbar" style={{width: '100%'}} aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"/>
+                      </div>
+                  </Card.Body>
+              </Card>
+              </div>
+            </div>
+            {/* <div className="row">
              <div className="form-group col-md-4">
               <label>Country:</label>
                 <input type="text" value={this.country} onChange={this.handleChangeCountry} className="form-control" />
                </div>
-              </div>
+              </div> */}
           </ModalBody>
           <ModalFooter>
-            <input type="submit" value="Submit" color="primary" className="btn btn-primary btn-sm" />
-            <Button color="danger" size="sm" onClick={this.toggle}>Cancel</Button>
+            {/* <input type="submit" value="Submit" color="primary" className="btn btn-primary btn-sm" /> */}
+            <Button color="danger" size="sm" onClick={this.toggle}>Close</Button>
           </ModalFooter>
           </form>
         </Modal>
--- src/Demo/Applications/DeliveryPool/DeliveryPool.js
+++ src/Demo/Applications/DeliveryPool/DeliveryPool.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
-import {Row, Col} from 'react-bootstrap';
+import {Row, Col, Table, Badge} from 'react-bootstrap';
 
 import Aux from "../../../hoc/_Aux";
+import DEMO from "../../../store/constant";
 import Card from "../../../App/components/MainCard";
+import ModalComponent from '../../../App/components/ModalComponent';
 
 class DeliveryPool extends Component {
     render() {
@@ -10,13 +12,53 @@ class DeliveryPool extends Component {
             <Aux>
                 <Row>
                     <Col>
-                        <Card title='Hello Card' isOption>
-                            <p>
-                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                            </p>
+                        <Card title='Delivery Pool' anotherOption>
+                            <Table striped responsive>
+                                <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Sender</th>
+                                    <th>Amount</th>
+                                    <th>Date</th>
+                                    <th>Product</th>
+                                    <th>Vendor</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Cynthia Ezechukwu</td>
+                                    <td>&#8358;1500</td>
+                                    <td>27/07/19 12:03pm</td>
+                                    <td>Standard</td>
+                                    <td>Daniel Abayomi</td>
+                                    <td><Badge variant="warning">Running</Badge></td>
+                                    <td><a href={DEMO.BLANK_LINK}><ModalComponent/></a></td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>Cynthia Ezechukwu</td>
+                                    <td>&#8358;1500</td>
+                                    <td>27/07/19 10:33am</td>
+                                    <td>Standard</td>
+                                    <td>Daniel Abayomi</td>
+                                    <td><Badge variant="warning">Running</Badge></td>
+                                    <td><a href={DEMO.BLANK_LINK}><ModalComponent/></a></td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">3</th>
+                                    <td>Cynthia Ezechukwu</td>
+                                    <td>&#8358;1500</td>
+                                    <td>27/07/19 13:25pm</td>
+                                    <td>Standard</td>
+                                    <td>Daniel Abayomi</td>
+                                    <td><Badge variant="warning">Running</Badge></td>
+                                    <td><a href={DEMO.BLANK_LINK}><ModalComponent/></a></td>
+                                </tr>
+                                </tbody>
+                            </Table>
                         </Card>
                     </Col>
                 </Row>
