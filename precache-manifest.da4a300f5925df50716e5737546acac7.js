4c27f941a54231b73c2551927e3ee15cd0bbaf5b
Ifeanyi Osinakayah Joseph <ifeanyi.Osinakayah@cousant.com>
2019-07-16 10:07:40 +0100

    Updates

---
+++ precache-manifest.da4a300f5925df50716e5737546acac7.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b26923b017bb50c5b4431f0a96526b",
+    "url": "/orula_admin_frontend/index.html"
+  },
+  {
+    "revision": "68b3ddab320c5e76c0ab",
+    "url": "/orula_admin_frontend/static/css/12.c307540f.chunk.css"
+  },
+  {
+    "revision": "4c1561d6662eac91b83c",
+    "url": "/orula_admin_frontend/static/css/2.4089b72e.chunk.css"
+  },
+  {
+    "revision": "c26db26731e2db8f3324",
+    "url": "/orula_admin_frontend/static/css/5.0126fdb5.chunk.css"
+  },
+  {
+    "revision": "84de076a93d01ffcc69c",
+    "url": "/orula_admin_frontend/static/css/6.bc73331f.chunk.css"
+  },
+  {
+    "revision": "2c1e2adcfca4bc1c8330",
+    "url": "/orula_admin_frontend/static/js/0.1ef38b62.chunk.js"
+  },
+  {
+    "revision": "3fb9f45a355d0a8dec68",
+    "url": "/orula_admin_frontend/static/js/1.1b64993a.chunk.js"
+  },
+  {
+    "revision": "276901144a9a9cb56cc9",
+    "url": "/orula_admin_frontend/static/js/10.e6dd7ebd.chunk.js"
+  },
+  {
+    "revision": "8cd031d8749b36c4dce9",
+    "url": "/orula_admin_frontend/static/js/11.9eaf65b8.chunk.js"
+  },
+  {
+    "revision": "68b3ddab320c5e76c0ab",
+    "url": "/orula_admin_frontend/static/js/12.e8f43581.chunk.js"
+  },
+  {
+    "revision": "32c1ff5e9299bd8fe917",
+    "url": "/orula_admin_frontend/static/js/13.70cf868c.chunk.js"
+  },
+  {
+    "revision": "853c1c6dc44ebea1c728",
+    "url": "/orula_admin_frontend/static/js/14.4fbe6ffc.chunk.js"
+  },
+  {
+    "revision": "7fe6f7bd12b2571c8f02",
+    "url": "/orula_admin_frontend/static/js/15.2fa58a19.chunk.js"
+  },
+  {
+    "revision": "a9a5d0775572003d6602",
+    "url": "/orula_admin_frontend/static/js/16.3d44f32f.chunk.js"
+  },
+  {
+    "revision": "66fb061ccdd85e93b7e9",
+    "url": "/orula_admin_frontend/static/js/17.378098a6.chunk.js"
+  },
+  {
+    "revision": "532d28334e0ac6d2aa4c",
+    "url": "/orula_admin_frontend/static/js/18.89b07ece.chunk.js"
+  },
+  {
+    "revision": "22c056965f5b7128997c",
+    "url": "/orula_admin_frontend/static/js/19.0397676a.chunk.js"
+  },
+  {
+    "revision": "4c1561d6662eac91b83c",
+    "url": "/orula_admin_frontend/static/js/2.78ca109e.chunk.js"
+  },
+  {
+    "revision": "25a25dacecdffc3409de",
+    "url": "/orula_admin_frontend/static/js/20.d1d32d7d.chunk.js"
+  },
+  {
+    "revision": "572be659805fa0810dd7",
+    "url": "/orula_admin_frontend/static/js/21.62184f42.chunk.js"
+  },
+  {
+    "revision": "7629b969f080fea55a18",
+    "url": "/orula_admin_frontend/static/js/22.bd4867d3.chunk.js"
+  },
+  {
+    "revision": "68556c71a378294f533f",
+    "url": "/orula_admin_frontend/static/js/23.f7d2a61f.chunk.js"
+  },
+  {
+    "revision": "e5a11cc20b82bb29b4db",
+    "url": "/orula_admin_frontend/static/js/24.4a994c5b.chunk.js"
+  },
+  {
+    "revision": "4db28ee110e9703fe147",
+    "url": "/orula_admin_frontend/static/js/25.f03a2ad8.chunk.js"
+  },
+  {
+    "revision": "589f44260ba2109d67be",
+    "url": "/orula_admin_frontend/static/js/26.42802f0d.chunk.js"
+  },
+  {
+    "revision": "ee351ffd9a2a0fb2cb7d",
+    "url": "/orula_admin_frontend/static/js/27.cf329dff.chunk.js"
+  },
+  {
+    "revision": "a44728e35a2e86242bcf",
+    "url": "/orula_admin_frontend/static/js/28.a5a7edbd.chunk.js"
+  },
+  {
+    "revision": "7b2481c93a5d3135c556",
+    "url": "/orula_admin_frontend/static/js/29.08e2e267.chunk.js"
+  },
+  {
+    "revision": "c5182cbd14cc10c9523a",
+    "url": "/orula_admin_frontend/static/js/30.5055f685.chunk.js"
+  },
+  {
+    "revision": "a9420cb658836aada888",
+    "url": "/orula_admin_frontend/static/js/31.17c867f6.chunk.js"
+  },
+  {
+    "revision": "e9185d5b930ae9d20db9",
+    "url": "/orula_admin_frontend/static/js/32.ec5766b4.chunk.js"
+  },
+  {
+    "revision": "65d71918f38fe10287a1",
+    "url": "/orula_admin_frontend/static/js/33.6d67faf9.chunk.js"
+  },
+  {
+    "revision": "1958e091be71939a0bc7",
+    "url": "/orula_admin_frontend/static/js/34.0839b6f0.chunk.js"
+  },
+  {
+    "revision": "44ed5c98f3dd140cd81d",
+    "url": "/orula_admin_frontend/static/js/35.ca024980.chunk.js"
+  },
+  {
+    "revision": "53aeafda5f82d84b59e4",
+    "url": "/orula_admin_frontend/static/js/36.994bdb49.chunk.js"
+  },
+  {
+    "revision": "8745e3bd4e29cb22c2fb",
+    "url": "/orula_admin_frontend/static/js/37.fc39efa8.chunk.js"
+  },
+  {
+    "revision": "c26db26731e2db8f3324",
+    "url": "/orula_admin_frontend/static/js/5.5cdb0f73.chunk.js"
+  },
+  {
+    "revision": "84de076a93d01ffcc69c",
+    "url": "/orula_admin_frontend/static/js/6.c586f561.chunk.js"
+  },
+  {
+    "revision": "54c382aadce865af0508",
+    "url": "/orula_admin_frontend/static/js/7.2486b5e4.chunk.js"
+  },
+  {
+    "revision": "4b9bebc7e1571b03a6b6",
+    "url": "/orula_admin_frontend/static/js/8.5fe4230f.chunk.js"
+  },
+  {
+    "revision": "480f2bf29b25ca0a80c8",
+    "url": "/orula_admin_frontend/static/js/9.d7860bed.chunk.js"
+  },
+  {
+    "revision": "17ff13885bd92110de48",
+    "url": "/orula_admin_frontend/static/js/main.f54e865c.chunk.js"
+  },
+  {
+    "revision": "5b6fdec1ad6f4fd1e9d3",
+    "url": "/orula_admin_frontend/static/js/runtime~main.b809b592.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/orula_admin_frontend/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/orula_admin_frontend/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/orula_admin_frontend/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/orula_admin_frontend/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/orula_admin_frontend/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
