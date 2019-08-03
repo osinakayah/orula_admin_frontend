9bd93dc59b96e09f0146c3528f843b32f9d9279d
Ifeanyi Osinakayah Joseph <ifeanyiosinakayah15@gmail.com>
2019-08-03 14:34:38 +0100

    Refactored api format

--- src/config.js
+++ src/config.js
@@ -20,11 +20,11 @@ export default {
     navListTitleColor: 'title-default', // title-default, (title-blue, title-red, title-purple, title-lightblue, title-dark are not available in lite version)
     navListTitleHide: false, // not available in lite version
     configBlock: false, // not available in lite version
-    layout6Background : 'linear-gradient(to right, #A445B2 0%, #D41872 52%, #FF0066 100%)', // used only for pre-layout = layout-6
-    layout6BackSize : '', // used only for pre-layout = layout-6,
+    layout6Background: 'linear-gradient(to right, #A445B2 0%, #D41872 52%, #FF0066 100%)', // used only for pre-layout = layout-6
+    layout6BackSize: '', // used only for pre-layout = layout-6,
     debug: true,
     authTokenLocalStorage: 'authTokenLocalStorage',
-    baseUrl:'http://localhost:3000/api/v1/',
+    baseUrl: 'http://localhost:3000/api/v1/',
 
     DELIVERY_INITIATED: 0,
     DELIVERY_ITEM_PICKED: 1,
@@ -32,7 +32,7 @@ export default {
     DELIVERY_COMPLETED: 4,
 
     DELIVERY_RECOVERY_INITIATED: 10,
-    DELIVERY_RECOVERY_COMPLETED:14,
+    DELIVERY_RECOVERY_COMPLETED: 14,
     VERIFIED_CUSTOMERS: 1,
     UNVERIFIED_CUSTOMERS: 0
 };
